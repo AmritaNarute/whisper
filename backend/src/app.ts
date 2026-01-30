ac8e877f9e1167e6b813edb7580a14eb54f161c6
DWebPixel <amruta.narute@dwebpixel.com>
2026-01-30 12:29:59 +0530

    Added Backend Controller

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,18 @@
 import express from 'express';
 import authRoutes from './routes/authRoutes';
 import chatRoutes from './routes/chatRoutes';
+import { clerkMiddleware } from '@clerk/express'
 import messageRoutes from './routes/messageRoutes';
 import userRoutes from './routes/userRoute';
+import { errorHandler } from './middleware/errorHandler';
 
 const app = express();
 
 app.use(express.json())
 
 const PORT = process.env.PORT || 3000
+
+app.use(express.json());
 
 app.get("/health", (req, res) => {
     res.json({ status: "ok", message: "server is running" })
@@ -18,5 +22,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/chats", chatRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
+
+app.use(errorHandler);
 
 export default app;
